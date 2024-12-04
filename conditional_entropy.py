24b263d36e53823d3ce5d80bcee9b4b698e7659a
Jeremy Couthures <jeremy.couthures@lapp.in2p3.fr>
2024-12-04 14:23:16 +0100

    Update docstring

--- conditional_entropy.py
+++ conditional_entropy.py
@@ -718,7 +718,7 @@ def entropy_histogram(data, bins=10):
 def conditional_entropy_histogram(data_y, data_x, bins=2):
     """
     Estimate conditional entropy H(Y | X) using a histogram-based approach.
-    :param data_y: Numpy array of shape (n_samples, 1), the target variable Y
+    :param data_y: Numpy array of shape (n_samples, n_features), the target variable Y
     :param data_x: Numpy array of shape (n_samples, n_features), the conditioning variables X
     :param bins: Number of bins for the histogram
     :return: Estimated conditional entropy H(Y | X)
